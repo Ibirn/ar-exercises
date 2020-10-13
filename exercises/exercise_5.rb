6930ace50285dfdc79ae76ccd1fc97adcbd46ede
Ibirn <kmcricciuti@gmail.com>
2020-10-13 01:03:52 +0000

    Add aggregate functions

--- exercises/exercise_5.rb
+++ exercises/exercise_5.rb
@@ -7,4 +7,7 @@ require_relative './exercise_4'
 puts "Exercise 5"
 puts "----------"
 
+puts "TOTAL REV: $#{Store.sum(:annual_revenue)}"
+puts "AVG REV: $#{Store.average(:annual_revenue)}"
+puts "STORES ABOVE 1M: #{Store.where("annual_revenue > ?", 1000000).size}"
 # Your code goes here ...
